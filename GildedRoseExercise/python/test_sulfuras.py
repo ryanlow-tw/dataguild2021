59486e5fd21afa01c515941a427c56868ecc9b7d
Ryan Low <kyzersoz@gmail.com>
2021-07-06 15:49:29 +0800

    edit name of test case

--- GildedRoseExercise/python/test_sulfuras.py
+++ GildedRoseExercise/python/test_sulfuras.py
@@ -5,7 +5,7 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_should_reduce_quality_by_0_when_quality_starts_at_20_and_item_is_foo(self):
+    def test_should_reduce_quality_by_0_when_quality_starts_at_20_and_item_is_sulfuras(self):
         items = [Item("Sulfuras, Hand of Ragnaros", 0, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
