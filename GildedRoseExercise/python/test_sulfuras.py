212140222c0bef80a11514ddade671e99825b599
Ryan Low <kyzersoz@gmail.com>
2021-07-06 15:47:55 +0800

    add test file for sulfuras item

--- GildedRoseExercise/python/test_sulfuras.py
+++ GildedRoseExercise/python/test_sulfuras.py
@@ -5,11 +5,11 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_should_reduce_quality_by_0_when_quality_starts_at_0_and_item_is_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_should_reduce_quality_by_0_when_quality_starts_at_20_and_item_is_foo(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(0, items[0].quality)
+        self.assertEquals(20, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
