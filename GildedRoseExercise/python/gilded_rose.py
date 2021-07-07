bb05f790c4ffabb9364446b06105ab05b4fe090c
Ryan Low <kyzersoz@gmail.com>
2021-07-07 09:49:19 +0800

    remove check for item quality below 0

--- GildedRoseExercise/python/gilded_rose.py
+++ GildedRoseExercise/python/gilded_rose.py
@@ -10,8 +10,7 @@ class GildedRose(object):
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    item.quality = item.quality - 1
+                item.quality = item.quality - 1
             else:
                 item.quality = item.quality + 1
                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
@@ -23,13 +22,13 @@ class GildedRose(object):
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            item.quality = item.quality - 1
+                        item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
                     item.quality = item.quality + 1
             item.quality = min(item.quality, 50)
+            item.quality = max(item.quality, 0)
 
 
 class Item:
