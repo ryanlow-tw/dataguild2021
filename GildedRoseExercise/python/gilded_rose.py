ed7c38880e475cd17d385c36e63ffc58224fc2bf
Ryan Low <kyzersoz@gmail.com>
2021-07-07 09:46:21 +0800

    remove check for item quaility to be below 50

--- GildedRoseExercise/python/gilded_rose.py
+++ GildedRoseExercise/python/gilded_rose.py
@@ -13,13 +13,12 @@ class GildedRose(object):
                 if item.quality > 0:
                     item.quality = item.quality - 1
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11 and item.quality < 50:
-                            item.quality = item.quality + 1
-                        if item.sell_in < 6 and item.quality < 50:
-                            item.quality = item.quality + 1
+                item.quality = item.quality + 1
+                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.sell_in < 11:
+                        item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        item.quality = item.quality + 1
             item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
@@ -29,8 +28,8 @@ class GildedRose(object):
                     else:
                         item.quality = item.quality - item.quality
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    item.quality = item.quality + 1
+            item.quality = min(item.quality, 50)
 
 
 class Item:
