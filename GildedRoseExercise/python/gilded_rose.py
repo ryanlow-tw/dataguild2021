f09b32a7f16eb6a9451824f6af671a3b26cc02b9
Ryan Low <kyzersoz@gmail.com>
2021-07-07 09:54:32 +0800

    reduce verbosity of code

--- GildedRoseExercise/python/gilded_rose.py
+++ GildedRoseExercise/python/gilded_rose.py
@@ -9,24 +9,29 @@ class GildedRose(object):
         for item in self.items:
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
+        
+
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                item.quality = item.quality - 1
+                item.quality -= 1
             else:
-                item.quality = item.quality + 1
+                item.quality += 1
                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
                     if item.sell_in < 11:
-                        item.quality = item.quality + 1
+                        item.quality += 1
                     if item.sell_in < 6:
-                        item.quality = item.quality + 1
+                        item.quality += 1
+
             item.sell_in = item.sell_in - 1
+
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        item.quality = item.quality - 1
+                        item.quality -= 1
                     else:
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                 else:
                     item.quality = item.quality + 1
+
             item.quality = min(item.quality, 50)
             item.quality = max(item.quality, 0)
 
