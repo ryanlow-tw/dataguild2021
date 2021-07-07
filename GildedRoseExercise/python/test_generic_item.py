ef9b1c7a41ecf33d0c0925c037b9d7b7167f5df4
Ryan Low <kyzersoz@gmail.com>
2021-07-07 15:09:44 +0800

    rename generic_item to "Generic Item"

--- GildedRoseExercise/python/test_generic_item.py
+++ GildedRoseExercise/python/test_generic_item.py
@@ -6,25 +6,25 @@ from gilded_rose import Item, GildedRose
 
 class GenericItemTest(unittest.TestCase):
     def test_should_reduce_quality_by_0_when_quality_starts_at_0_and_item_is_generic_item(self):
-        items = [Item("generic_item", 0, 0)]
+        items = [Item("Generic Item", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(0, items[0].quality)
 
     def test_should_reduce_quality_by_2_when_quality_starts_at_2_and_item_is_generic_item(self):
-        items = [Item("generic_item", 0, 2)]
+        items = [Item("Generic Item", 0, 2)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(0, items[0].quality)
 
     def test_should_reduce_quality_by_1_when_quality_starts_at_1_and_item_is_generic_item(self):
-        items = [Item("generic_item", 0, 1)]
+        items = [Item("Generic Item", 0, 1)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(0, items[0].quality)
 
     def test_should_reduce_quality_by_2_when_quality_starts_at_20_and_item_is_generic_item(self):
-        items = [Item("generic_item", 0, 20)]
+        items = [Item("Generic Item", 0, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(18, items[0].quality)
