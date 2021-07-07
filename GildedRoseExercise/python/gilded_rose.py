40d72416e7aa1407de19e29d8d2e09ff30b8c89a
Ryan Low <kyzersoz@gmail.com>
2021-07-07 10:17:45 +0800

    refactor if-else statement for backstage pass

--- GildedRoseExercise/python/gilded_rose.py
+++ GildedRoseExercise/python/gilded_rose.py
@@ -15,9 +15,9 @@ class GildedRose(object):
             else:
                 item.quality += 1
                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        item.quality += 1
                     if item.sell_in < 6:
+                        item.quality += 2
+                    elif item.sell_in < 11:
                         item.quality += 1
 
             item.sell_in = item.sell_in - 1
