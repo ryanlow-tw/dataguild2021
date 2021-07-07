783a433f5e2e9a59c0fe0a34b6c341adeffc7b3d
Ryan Low <kyzersoz@gmail.com>
2021-07-07 15:19:39 +0800

    remove whitespace

--- GildedRoseExercise/python/gilded_rose.py
+++ GildedRoseExercise/python/gilded_rose.py
@@ -11,9 +11,6 @@ class GildedRose(object):
                 continue
             else:
                 item = self._update_quality_handler(item)
-            item.sell_in -= 1
-            item.quality = min(item.quality, 50)
-            item.quality = max(item.quality, 0)
     
     def _update_quality_handler(self,item):
         if item.name == 'Generic Item':
@@ -24,6 +21,9 @@ class GildedRose(object):
             item = self._update_concert_ticket_quality(item)
         elif item.name == 'Conjured Item':
             item = self._update_conjured_item_quality(item)
+        item.sell_in -= 1
+        item.quality = min(item.quality, 50)
+        item.quality = max(item.quality, 0)
         return item
             
     def _update_generic_item_quality(self, item):
@@ -54,12 +54,6 @@ class GildedRose(object):
         else:
             item.quality -= 2
         return item
-
-
-
-
-
-
 
 class Item:
     def __init__(self, name, sell_in, quality):
