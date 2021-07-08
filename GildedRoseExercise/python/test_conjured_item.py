9cd96133c11c942ef5e268943a4ee3f7a89cb393
Ryan Low <kyzersoz@gmail.com>
2021-07-08 10:18:00 +0800

    change method used in test cases from self.assertEquals to self.assertEqual

--- GildedRoseExercise/python/test_conjured_item.py
+++ GildedRoseExercise/python/test_conjured_item.py
@@ -9,19 +9,19 @@ class ConjuredItemTest(unittest.TestCase):
         items = [Item("Conjured Item", 10, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(18, items[0].quality)
+        self.assertEqual(18, items[0].quality)
 
     def test_should_reduce_quality_by_4_when_quality_starts_at_0_and_sell_in_date_is_not_0_and_item_is_conjured_item(self):
         items = [Item("Conjured Item", 0, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(16, items[0].quality)
+        self.assertEqual(16, items[0].quality)
 
     def test_should_reduce_quality_by_2_when_quality_starts_at_2_and_sell_in_date_is_not_0_and_item_is_conjured_item(self):
         items = [Item("Conjured Item", 0, 2)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(0, items[0].quality)
+        self.assertEqual(0, items[0].quality)
   
 if __name__ == '__main__':
     unittest.main()
--- GildedRoseExercise/python/test_generic_item.py
+++ GildedRoseExercise/python/test_generic_item.py
@@ -9,25 +9,25 @@ class GenericItemTest(unittest.TestCase):
         items = [Item("Generic Item", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(0, items[0].quality)
+        self.assertEqual(0, items[0].quality)
 
     def test_should_reduce_quality_by_2_when_quality_starts_at_2_and_item_is_generic_item(self):
         items = [Item("Generic Item", 0, 2)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(0, items[0].quality)
+        self.assertEqual(0, items[0].quality)
 
     def test_should_reduce_quality_by_1_when_quality_starts_at_1_and_item_is_generic_item(self):
         items = [Item("Generic Item", 0, 1)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(0, items[0].quality)
+        self.assertEqual(0, items[0].quality)
 
     def test_should_reduce_quality_by_2_when_quality_starts_at_20_and_item_is_generic_item(self):
         items = [Item("Generic Item", 0, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(18, items[0].quality)
+        self.assertEqual(18, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
